30a919c5313fb013244911437bd9fecb8c9eeb0e
bayuguna11 <putubayuguna@gmail.com>
2025-03-06 15:11:53 +0800

    update

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -1,9 +1,12 @@
 import { APP_VERSION } from "@/utils/constanta/setting";
+import { Link } from "react-router-dom";
 
 const BFooter = () => {
   return (
     <div className="py-4 px-4 border-t flex justify-between">
-      <span className="text-xs">Created By: putubayuguna@gmail.com</span>
+      <Link to="mailto:putubayuguna@gmail.com">
+        <span className="text-xs">putubayuguna@gmail.com</span>
+      </Link>
       <span className="text-xs">Version {APP_VERSION}</span>
     </div>
   );
